b754d6d272325814f4bee3791bee7b6db933328d
Bjorn Jensen-Urstad <bjorn.jensen.urstad@gmail.com>
2014-02-03 17:57:28 +0100

    Marginally better

--- leader2.rb
+++ leader2.rb
@@ -3,51 +3,70 @@ require 'zookeeper'
 require 'json'
 
 class AsMaster
+  @@root = '/ELECTION'
+
   def initialize(service)
     @service = service
-    @zk      = Zookeeper.new('localhost:2181')
+    @zk      = connect()
     @zk.create(:path => '/ELECTION')
-    res = @zk.create(:path => "/ELECTION/#{@service}_", :ephemeral => true, :sequence => true)
-    @seqno_me = res[:path].split('_')[1]
   end
 
   def run(&x)
+    my_seqno = create_me()
+    become_leader(my_seqno)
+    Thread.abort_on_exception = true
+    t = Thread.new do
+      x.call
+    end
+    while res = @zk.stat(:path => "#{@@root}/#{@service}_#{my_seqno}")
+      break unless res[:stat].exists?
+      break unless t.alive?
+      sleep 1
+    end
+  end
+
+  private
+
+  def become_leader(my_seqno)
     while true
-      seqno_mon = nil
-      @zk.get_children(:path => '/ELECTION')[:children].each{|n|
-        (service, seqno) = n.split('_')
-        if service==@service && seqno_mon!=nil && seqno > seqno_mon && seqno < @seqno_me
-          seqno_mon = seqno
-        elsif service==@service && seqno_mon==nil && seqno < @seqno_me
-          seqno_mon = seqno
+      waiting = @zk.get_children(:path => @@root)[:children].select { |e|
+        e.split('_')[0] == @service
+      }
+      monitor = waiting.inject(nil) { |monitor, e|
+        seqno = e.split('_')[1]
+        if monitor && seqno > monitor && seqno < my_seqno
+          seqno
+        elsif !monitor && seqno < my_seqno
+          seqno
+        else
+          monitor
         end
       }
 
-      if seqno_mon
-        puts "monitoring " + seqno_mon
-        s = "/ELECTION/#{@service}_" + seqno_mon
-        q = Queue.new
-        w = Zookeeper::Callbacks::WatcherCallback.new{
-          q.push(:done)
-        }
-        res = @zk.stat(:path => s, :watcher => w)
-        if res[:stat].exists?
-          q.pop()
-        end
+      if monitor
+        q   = Queue.new
+        res = @zk.stat(:path    => "#{@@root}/#{@service}_#{monitor}",
+                       :watcher => Zookeeper::Callbacks::WatcherCallback.new{
+                         q.push(:done)
+                       })
+        q.pop() if res[:stat].exists?
       else
-        Thread.abort_on_exception = true
-        t = Thread.new do
-          x.call
-        end
-        while res = @zk.stat(:path => "/ELECTION/#{@service}_" + @seqno_me)
-          break unless res[:stat].exists?
-          sleep 1
-        end
+        break
       end
     end
   end
-end
 
+  def connect()
+    Zookeeper.new('192.168.0.20:2181')
+  end
+
+  def create_me()
+    r = @zk.create(:path => "/ELECTION/#{@service}_",
+                   :ephemeral => true,
+                   :sequence  => true)
+    r[:path].split('_')[1]
+  end
+end
 
 m = AsMaster.new('service')
 m.run do
